818d831e634b82193114d1b7794b9acefdd11abd
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-07-17 07:54:55 +0530

    Added authentication with sating and JWT token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Saurabhisagoodb$oy";
 
 // Create a User using POST "/api/auth/createuser". No login required
 router.post(
@@ -25,9 +29,8 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check whether the user with this email exists already
-
     try {
+      // Check whether the user with this email exists already
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res
@@ -35,14 +38,29 @@ router.post(
           .json({ error: "Sorry a user with this email already exists" });
       }
 
+      // Hashing and salting the password
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       // Create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
 
-      res.json(user);
+      // Adding JWT token authentication
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      // here we don't need to await bcz sign() is synchronous method, we will directly get the data
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // console.log(authToken);
+      res.json({ authToken }); // bcz we are using ES^we dont have to write res.json({authToken: authToken})
+
+      // res.json(user);
 
       // catch errors
     } catch (error) {
