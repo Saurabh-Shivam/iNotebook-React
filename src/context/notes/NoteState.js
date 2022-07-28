0e792710d658163e939fab51b82997fa73a2ac27
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-07-28 18:02:24 +0530

    Used the useLocation and added form in the Home.js file

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,21 +1,20 @@
-import { useState } from "react";
+// import { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Saurabh",
-    class: "12",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({ name: "Shivam", class: "10" });
-    }, 1000);
-  };
+  //   const s1 = {
+  //     name: "Saurabh",
+  //     class: "12",
+  //   };
+  //   const [state, setState] = useState(s1);
+  //   const update = () => {
+  //     setTimeout(() => {
+  //       setState({ name: "Shivam", class: "10" });
+  //     }, 1000);
+  //   };
   return (
-    <noteContext.Provider value={{ state: state, update: update }}>
-      {props.children}
-    </noteContext.Provider>
+    // <noteContext.Provider value={{ state: "state", update: "update" }}>
+    <noteContext.Provider value={[]}>{props.children}</noteContext.Provider>
   );
 };
 
