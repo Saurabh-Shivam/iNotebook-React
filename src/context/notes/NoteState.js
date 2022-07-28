912bf8abc91b099b0308618b0f556ee0f318ab03
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-07-28 17:34:57 +0530

    Learned about react context api and implemented it

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import noteContext from "./noteContext";
+
+const NoteState = (props) => {
+  const s1 = {
+    name: "Saurabh",
+    class: "12",
+  };
+  const [state, setState] = useState(s1);
+  const update = () => {
+    setTimeout(() => {
+      setState({ name: "Shivam", class: "10" });
+    }, 1000);
+  };
+  return (
+    <noteContext.Provider value={{ state: state, update: update }}>
+      {props.children}
+    </noteContext.Provider>
+  );
+};
+
+export default NoteState;
---
+++ src/context/notes/noteContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+// Context will hold all the states related to our notes
+
+const noteContext = createContext();
+
+export default noteContext;
