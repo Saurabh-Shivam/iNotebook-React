3d792a24e04e0579036a5d85d8bc2a521d234ecd
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2022-07-29 18:00:38 +0530

    Adding AddNote component to iNotebook and adding the notes locally using submit button

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -69,9 +69,54 @@ const NoteState = (props) => {
     },
   ];
   const [notes, setNotes] = useState(notesInitial);
+
+  // Add a Note
+
+  const addNote = (title, description, tag) => {
+    console.log("Adding a new note");
+    // TODO: API Call
+    const note = {
+      _id: "62e282ed18t11b859g0aac40ecded",
+      user: "62d37268d3104aacef1c99ea",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2022-07-28T12:37:01.398Z",
+      __v: 0,
+    };
+    // NOTE:-> concat returns an array whereas push updates an array
+    setNotes(notes.concat(note));
+  };
+
+  // // Add a Note
+  // const addNote = (title, description, tag) => {
+  //   // TODO: API Call
+  //   console.log("Adding a new note");
+  //   const note = {
+  //     _id: "61322f119553781a8ca8d0e08",
+  //     user: "62d37268d3104aacef1c99ea",
+  //     title: title,
+  //     description: description,
+  //     tag: tag,
+  //     date: "2022-07-28T12:37:01.398Z",
+  //     __v: 0,
+  //   };
+  //   setNotes(notes.concat(note));
+  // };
+
+  // Delete a Note
+
+  const deleteNote = (id) => {};
+
+  // Edit a Note
+
+  const editNote = (id) => {};
+
   return (
     // <noteContext.Provider value={{ state: "state", update: "update" }}>
-    <noteContext.Provider value={{ notes, setNotes }}>
+    <noteContext.Provider
+      value={{ notes, setNotes, addNote, deleteNote, editNote }}
+    >
       {props.children}
     </noteContext.Provider>
   );
